b16d85740a8f809c3b2e2225a7daa3696729e59b
AlekseiMakonovitskii <alexmakondev@gmail.com>
2023-08-17 18:25:11 +0300

    feat: hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgY0ncNv3SUG4HjdOHGL23tSHtSZ_s-Lo",
+  authDomain: "study-words-daaily.firebaseapp.com",
+  projectId: "study-words-daaily",
+  storageBucket: "study-words-daaily.appspot.com",
+  messagingSenderId: "147515888307",
+  appId: "1:147515888307:web:f1ee4f67741c839afb56c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
